8fa7e68b0333a11561d78e5325f53ef6fb1a2d43
ERICK <50150389+erickdiias@users.noreply.github.com>
2025-03-27 22:30:58 -0300

    Edit

--- RTL/A429_Tx.vhd
+++ RTL/A429_Tx.vhd
@@ -13,15 +13,15 @@ entity A429_Tx is
         tranmssion_speed : integer := 100_000;
         frame_label : std_logic_vector(7 downto 0) := X"4F"; -- 01001111 (79)
         sdi : std_logic_vector(1 downto 0) := "00"; -- 00-All Systems, 01-System1, 10-System2, 11-System3
-        ssm : std_logic_vector(1 downto 0) := "00"; -- Normal
+        ssm : std_logic_vector(1 downto 0) := "00" -- Normal
     );
     port(
         clk, rst : in std_logic;
-        data_in : in std_logic_vector(18 downto 0); -- Form discret
+        data_in : in std_logic_vector(18 downto 0) -- Form discret
     );
 end entity;
 
 architecture main of A429_Tx is
-    constant clk_div: integer := clk_freq / tranmssion_speed;
+    constant clk_div: natural := clk_freq / tranmssion_speed;
 begin
 end architecture;
